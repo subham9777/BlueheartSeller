c6eb830ab707b35a887976aad2317fe5564b1fe6
subham9777 <subhamagrawal861@gmail.com>
2017-09-12 11:27:24 +0530

    Added Image Upload

--- app/src/main/java/seller/blueheart/com/blueheartseller/MainActivity.java
+++ app/src/main/java/seller/blueheart/com/blueheartseller/MainActivity.java
@@ -1,24 +1,295 @@
 package seller.blueheart.com.blueheartseller;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.R.id.list;
+import static seller.blueheart.com.blueheartseller.R.id.editText;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
     Button gen, scan;
+    private static final int PICK_IMAGE_REQUEST = 234;
+
+    //view objects
+    private Button buttonChoose;
+    private Button buttonUpload;
+    private EditText editTextName,code_scan;
+    private TextView textViewShow;
+    private ImageView imageView,scan_image;
+
+    //uri to store file
+    private Uri filePath;
+
+    //firebase objects
+    private StorageReference storageReference;
+    private DatabaseReference mDatabase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        scan = (Button) findViewById(R.id.scan);
-        scan.setOnClickListener(new View.OnClickListener() {
+        final Activity activity = this;
+
+        buttonChoose = (Button) findViewById(R.id.buttonChoose);
+        buttonUpload = (Button) findViewById(R.id.buttonUpload);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        editTextName = (EditText) findViewById(editText);
+        textViewShow = (TextView) findViewById(R.id.textViewShow);
+        scan_image = (ImageView) findViewById(R.id.imageView1);
+        code_scan = (EditText) findViewById(R.id.editText1) ;
+
+        storageReference = FirebaseStorage.getInstance().getReference();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        buttonChoose.setOnClickListener(this);
+        buttonUpload.setOnClickListener(this);
+        textViewShow.setOnClickListener(this);
+        scan_image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent rIntent = new Intent(MainActivity.this, ReaderActivity.class);
-                startActivity(rIntent);
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+                integrator.setPrompt("Scan");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(true);
+                integrator.setBarcodeImageEnabled(true);
+                integrator.initiateScan();
             }
         });
+        Spinner sp1 = (Spinner) findViewById(R.id.spinner1);
+        Spinner sp2 = (Spinner) findViewById(R.id.spinner2);
+        sp1.setOnItemSelectedListener(this);
+        String s1 = String.valueOf(sp1.getSelectedItem());
+        if(s1.contentEquals("Automobile")) {
+            List<String> list = new ArrayList<String>();
+            list.add("AUDI");
+            list.add("ROLLS ROYCE ");
+            list.add("BUGATTI ");
+            list.add("FERRARI");
+            ArrayAdapter<String> dataAdapter1 = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,list);
+            dataAdapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            sp2.setAdapter(dataAdapter1);
+        }
+        if(s1.contentEquals("Business Services")) {
+            List<String> list1 = new ArrayList<String>();
+            list1.add("ZOHO");
+            list1.add("IBM");
+            list1.add("HCL");
+            list1.add("HP");
+            ArrayAdapter<String> dataAdapter2 = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,list);
+            dataAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            dataAdapter2.notifyDataSetChanged();
+            sp2.setAdapter(dataAdapter2);
+        }
+        if(s1.contentEquals("Computers")) {
+            List<String> list2 = new ArrayList<String>();
+            list2.add("HP");
+            list2.add("DELL");
+            list2.add("LENOVO ");
+            list2.add("COMPAQ");
+            ArrayAdapter<String> dataAdapter3 = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,list);
+            dataAdapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            dataAdapter3.notifyDataSetChanged();
+            sp2.setAdapter(dataAdapter3);
+        }
+        if(s1.contentEquals("Education")) {
+            List<String> list3 = new ArrayList<String>();
+            list3.add("SRM UNVERSITY");
+            list3.add("VIT UNIVERSITY");
+            list3.add("IIT MADRAS ");
+            list3.add("ANNA UNIVERSITY");
+            ArrayAdapter<String> dataAdapter4 = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,list);
+            dataAdapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            dataAdapter4.notifyDataSetChanged();
+            sp2.setAdapter(dataAdapter4);
+        }
+        if(s1.contentEquals("Personal")) {
+            List<String> list4 = new ArrayList<String>();
+            list4.add("PEN ");
+            list4.add("PENCIL");
+            list4.add("ERASER ");
+            list4.add("SHARPENER");
+            ArrayAdapter<String> dataAdapter5 = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,list);
+            dataAdapter5.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            dataAdapter5.notifyDataSetChanged();
+            sp2.setAdapter(dataAdapter5);
+        }
+        if(s1.contentEquals("Travel")) {
+            List<String> list5 = new ArrayList<String>();
+            list5.add("DUBAI");
+            list5.add("HONG KONG ");
+            list5.add("U.S.A. ");
+            list5.add("GERMANY");
+            ArrayAdapter<String> dataAdapter6 = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,list);
+            dataAdapter6.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            dataAdapter6.notifyDataSetChanged();
+            sp2.setAdapter(dataAdapter6);
+        }
+    }
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null){
+            if(result.getContents()==null){
+                Toast.makeText(this, "You cancelled the scanning", Toast.LENGTH_LONG).show();
+            }
+            else {
+                Toast.makeText(this, result.getContents(),Toast.LENGTH_LONG).show();
+                EditText editText1 = (EditText) findViewById(R.id.editText1);
+                editText1.setText(result.getContents());
+            }
+        }
+        else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getFileExtension(Uri uri) {
+        ContentResolver cR = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cR.getType(uri));
+    }
+
+    private void uploadFile() {
+        //checking if file is available
+        if (filePath != null) {
+            //displaying progress dialog while image is uploading
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("Uploading");
+            progressDialog.show();
+
+            //getting the storage reference
+            StorageReference sRef = storageReference.child(Constants.STORAGE_PATH_UPLOADS + System.currentTimeMillis() + "." + getFileExtension(filePath));
+
+            //adding the file to reference
+            sRef.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            //dismissing the progress dialog
+                            progressDialog.dismiss();
+
+                            //displaying success toast
+                            Toast.makeText(getApplicationContext(), "File Uploaded ", Toast.LENGTH_LONG).show();
+
+                            //creating the upload object to store uploaded image details
+
+                            Upload upload = new Upload(editTextName.getText().toString().trim(), taskSnapshot.getDownloadUrl().toString());
+                            //adding an upload to firebase database
+                            String uploadId = mDatabase.push().getKey();
+                            mDatabase.child(uploadId).setValue(upload);
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), exception.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            //displaying the upload progress
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred()) / taskSnapshot.getTotalByteCount();
+                            progressDialog.setMessage("Uploaded " + ((int) progress) + "%...");
+                            Toast.makeText(MainActivity.this, ""+taskSnapshot.getDownloadUrl(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        } else {
+            //display an error if no file is selected
+            Toast.makeText(this, "No file selected", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonChoose) {
+            showFileChooser();
+        } else if (view == buttonUpload) {
+            uploadFile();
+        } else if (view == textViewShow) {
+            startActivity(new Intent(this, ShowImagesActivity.class));
+        }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Are you sure you want to exit?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        MainActivity.this.finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+
     }
 }
